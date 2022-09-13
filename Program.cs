3fb006d074dabb19c37838b526d4dcd5aaa8127a
Slava <d-color.38@mail.ru>
2022-09-14 02:55:49 +0800

    dz29

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Укажите размер массива - " );
+int count = Convert.ToInt32(Console.ReadLine());
+int[] mass = new int[count];
+for (int i = 0; i < count; i++)
+    {
+    Console.WriteLine($"Введите элемент массива с индексом {i} - ");
+    mass[i] = Convert.ToInt32(Console.ReadLine());
+    } 
+Console.WriteLine($"Ваш массив: [{String.Join(",", mass)}]");
